2c7d194c95f15f3bb69bff3e0172be9930a72671
su-kun1899 <higedrum.coz@gmail.com>
2021-01-23 19:42:27 +0900

    :sparkles: bake ArticlesController
    bin/cake bake controller -v --actions=index Articles

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Articles Controller
+ *
+ * @property \App\Model\Table\ArticlesTable $Articles
+ * @method \App\Model\Entity\Article[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class ArticlesController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        $this->paginate = [
+            'contain' => ['Users'],
+        ];
+        $articles = $this->paginate($this->Articles);
+
+        $this->set(compact('articles'));
+    }
+}
---
+++ tests/TestCase/Controller/ArticlesControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\ArticlesController;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Controller\ArticlesController Test Case
+ *
+ * @uses \App\Controller\ArticlesController
+ */
+class ArticlesControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    protected $fixtures = [
+        'app.Articles',
+        'app.Users',
+        'app.ArticleTags',
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
