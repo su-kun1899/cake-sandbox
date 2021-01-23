afd4d548fb0d9e8d36d585bde90b007128a4848a
su-kun1899 <higedrum.coz@gmail.com>
2021-01-23 19:45:28 +0900

    :lipstick:

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -3,20 +3,25 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Model\Entity\Article;
+use App\Model\Table\ArticlesTable;
+use Cake\Datasource\ResultSetInterface;
+use Cake\Http\Response;
+
 /**
  * Articles Controller
  *
- * @property \App\Model\Table\ArticlesTable $Articles
- * @method \App\Model\Entity\Article[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ * @property ArticlesTable $Articles
+ * @method Article[]|ResultSetInterface paginate($object = null, array $settings = [])
  */
 class ArticlesController extends AppController
 {
     /**
      * Index method
      *
-     * @return \Cake\Http\Response|null|void Renders view
+     * @return Response|null|void Renders view
      */
-    public function index()
+    public function index(): ?Response
     {
         $this->paginate = [
             'contain' => ['Users'],
