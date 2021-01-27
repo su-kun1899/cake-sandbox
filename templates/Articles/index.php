18930c2a8aae2586bf0219a0dc06552991b31ccf
su-kun1899 <higedrum.coz@gmail.com>
2021-01-27 22:49:01 +0900

    :sparkles: add edit template

--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Model\Entity\Article;
 use App\View\AppView;
 use Cake\Collection\CollectionInterface;
@@ -14,16 +15,20 @@ use Cake\Collection\CollectionInterface;
     <tr>
         <th>タイトル</th>
         <th>作成日時</th>
+        <th>操作</th>
     </tr>
 
     <?php foreach ($articles as $article): ?>
-    <tr>
-        <td>
-            <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
-        </td>
-        <td>
-            <?= $article->created->format(DATE_RFC850) ?>
-        </td>
-    </tr>
+        <tr>
+            <td>
+                <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
+            </td>
+            <td>
+                <?= $article->created->format(DATE_RFC850) ?>
+            </td>
+            <td>
+                <?= $this->Html->link('編集', ['action' => 'edit', $article->slug]) ?>
+            </td>
+        </tr>
     <?php endforeach; ?>
 </table>
