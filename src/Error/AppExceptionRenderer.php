cbc6bca8816cbe0e154d0ecb66617eb399d17725
su-kun1899 <higedrum.coz@gmail.com>
2021-03-06 21:11:04 +0900

    :sparkles: add AppExceptionRenderer

---
+++ src/Error/AppExceptionRenderer.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Error;
+
+use Cake\Error\ExceptionRenderer;
+
+/**
+ * Class AppExceptionRenderer
+ *
+ * @package App\Error
+ */
+class AppExceptionRenderer extends ExceptionRenderer
+{
+}
